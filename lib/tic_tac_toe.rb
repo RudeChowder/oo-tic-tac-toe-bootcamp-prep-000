fb7aa3073fc745e9ff48612fa7d623efcdabad22
Stephen Crane <scrane88@gmail.com>
2020-01-18 16:50:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,17 +15,17 @@ class TicTacToe
   ]
   
   def display_board
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
   def input_to_index(input)
-    index = input.to_i - 1
+    @index = input.to_i - 1
   end
   
   def move(index, token = "X")
-    
+    @board[index] = token
 end
