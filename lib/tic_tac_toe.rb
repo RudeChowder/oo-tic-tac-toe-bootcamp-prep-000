0beed0617a1771a9667e0d164ec8449fe955867b
Stephen Crane <scrane88@gmail.com>
2020-01-18 17:04:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,5 +52,15 @@ class TicTacToe
   end
   
   def turn_count
+    count = 0
+    @board.each do |space|
+    if space == "X" || space == "O"
+      count += 1
+    end
+    count
+  end
+  
+  def current_player
     
+  end
 end
